2f08a96e587720c8c0d3c0364f19cc0335d3c658
Gaurav Singh <gs7august@gmail.com>
2026-04-10 17:47:57 +0530

    New commit

---
+++ server/configs/prisma.js
@@ -0,0 +1,15 @@
+import { Pool } from '@neondatabase/serverless';
+import { PrismaNeon } from '@prisma/adapter-neon';
+import { PrismaClient } from '@prisma/client';
+import ws from 'ws';
+neonConfig.webSocketConstructor = ws;
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaNeon(pool);
+const prisma = new PrismaClient({ adapter });
+
+if(process.env.NODE_ENV === 'development') global.prisma = prisma;
+
+export default prisma;
