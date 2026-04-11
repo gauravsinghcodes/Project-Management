b12415e8c5d661ee3143eaa7e5f52b5f155ec548
Gaurav Singh <gs7august@gmail.com>
2026-04-11 18:32:44 +0530

    newww

--- server/server.js
+++ server/server.js
@@ -16,7 +16,11 @@ app.use("/api/inngest", serve({ client: inngest, functions }));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+
+export default app;
 
 
 
