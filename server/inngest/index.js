752056e7b2c438370527184cdf647a3430d746b3
Gaurav Singh <gs7august@gmail.com>
2026-04-11 18:40:57 +0530

    newwww

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -5,14 +5,13 @@ export const inngest = new Inngest({ id: "project-management" });
 
 // Inngest Function to save user data to a database
 const syncUserCreation = inngest.createFunction(
-    { id: 'sync-user-from-clerk' },
-    { event: 'clerk/user.created' },
+    { id: 'sync-user-from-clerk', event: 'clerk/user.created' },
     async ({ event }) => {
         const { data } = event
         await prisma.user.create({
             data: {
                 id: data.id,
-                email: data?.email_addresses[0]?.email_addres,
+                email: data?.email_addresses[0]?.email_address,
                 name: data?.first_name + " " + data?.last_name,
                 image: data?.image_url,
             }
@@ -23,8 +22,7 @@ const syncUserCreation = inngest.createFunction(
 
 // Inngest Function to delete user from database
 const syncUserDeletion = inngest.createFunction(
-    { id: 'delete-user-with-clerk' },
-    { event: 'clerk/user.deleted' },
+    { id: 'delete-user-with-clerk', event: 'clerk/user.deleted' },
     async ({ event }) => {
         const { data } = event
         await prisma.user.delete({
@@ -38,8 +36,7 @@ const syncUserDeletion = inngest.createFunction(
 
 // Inngest Function to update user data in database
 const syncUserUpdation = inngest.createFunction(
-    { id: 'update-user-from-clerk' },
-    { event: 'clerk/user.updated' },
+    { id: 'update-user-from-clerk', event: 'clerk/user.updated' },
     async ({ event }) => {
         const { data } = event
         await prisma.user.update({
@@ -47,7 +44,7 @@ const syncUserUpdation = inngest.createFunction(
                 id: data.id
             },
             data: {
-                email: data?.email_addresses[0]?.email_addres,
+                email: data?.email_addresses[0]?.email_address,
                 name: data?.first_name + " " + data?.last_name,
                 image: data?.image_url,
             }
