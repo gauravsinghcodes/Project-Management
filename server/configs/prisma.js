ebc7ad78437109d162becc632799fe594a31776a
Gaurav Singh <gs7august@gmail.com>
2026-04-15 09:52:50 +0530

    yy

--- server/configs/prisma.js
+++ server/configs/prisma.js
@@ -1,4 +1,4 @@
-import { neonConfig, Pool } from '@neondatabase/serverless';
+import { neonConfig } from '@neondatabase/serverless';
 import { PrismaNeon } from '@prisma/adapter-neon';
 import { PrismaClient } from '@prisma/client';
 import ws from 'ws';
@@ -6,8 +6,8 @@ neonConfig.webSocketConstructor = ws;
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
-const pool = new Pool({ connectionString });
-const adapter = new PrismaNeon(pool);
+// const pool = new Pool({ connectionString });
+const adapter = new PrismaNeon({ connectionString });
 const prisma = global.prisma || new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV === 'development') global.prisma = prisma;
